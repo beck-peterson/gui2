ad484ee5a01bc4abe3b9bccccb9ddf74c37a235b
beck-peterson <56322362+beck-peterson@users.noreply.github.com>
2020-03-27 17:25:23 -0400

    added dog data structure, added some profile formatting

--- js/DataStructures.js
+++ js/DataStructures.js
@@ -1,20 +1,28 @@
 /* Created 3:00 PM 3/19/2020 */
 
-window.testPerson = new Person(new PublicInfo("Cesar", "Milan", "Dog Whisperer", "https://www.gstatic.com/tv/thumb/persons/503118/503118_v9_ba.jpg"), null, null, new PersonSettings(true, true), "", "");
+lola = new Dog(new GeneralInfo("Lola"));
+window.testPerson = new Person(new PublicInfo("Cesar", "Milan", "Dog Whisperer", "https://www.gstatic.com/tv/thumb/persons/503118/503118_v9_ba.jpg"), null, null, new PersonSettings(true, true), "", "", lola);
 
-function Person(publicInfo = null, privateInfo = null, accountInfo = null, personSettings = null, bio = "", pictures = null) {
+function Person(publicInfo = null, privateInfo = null, accountInfo = null, personSettings = null, bio = "", pictures = null, dogs = null) {
 	this.publicInfo = publicInfo != null ? publicInfo : new PublicInfo();
 	this.privateInfo = privateInfo != null ? privateInfo : new PrivateInfo();
 	this.accountInfo = accountInfo != null ? accountInfo : new AccountInfo();
 	this.personSettings = personSettings != null ? personSettings : new PersonSettings();
 	this.bio = bio;
 	this.pictures = pictures;
+	if (dogs == null) {
+		dogs = new Array();
+	} else if (Array.isArray(dogs)) {
+		this.dogs = dogs;
+	} else {
+		this.dogs = new Array(dogs);
+	}
 }
 
-function PublicInfo(firstName = "", lastName = "", organization = "", profilePicture = null) {
+function PublicInfo(firstName = "", lastName = "", organisation = "", profilePicture = null) {
 	this.firstName = firstName;
 	this.lastName = lastName;
-	this.organization = organization;
+	this.organisation = organisation;
 	this.profilePicture = profilePicture;
 }
 
@@ -33,13 +41,26 @@ function PersonSettings(breeder = false, visible = false) {
 	this.visible = visible;
 }
 
-function Dog(age = null, breed = "", city = "", dogPurpose = "", name = "", sex = "", state = "", zip = null) {
-	this.age = age;
-	this.breed = breed;
-	this.city = city;
-	this.dogPurpose = dogPurpose;
-	this.name = name;
-	this.sex = sex;
-	this.state = state;
-	this.zip = zip;
+function Dog(generalInfo = null, healthInfo = null, breedingInfo = null, sellingInfo = null) {
+	this.generalInfo = generalInfo != null ? generalInfo : new GeneralInfo();
+	this.healthInfo = healthInfo != null ? healthInfo : new HealthInfo();
+	this.breedingInfo = breedingInfo != null ? breedingInfo : new BreedingInfo();
+	this.sellingInfo = sellingInfo != null ? sellingInfo : new SellingInfo();
+}
+
+function GeneralInfo(firstName = "", lastName = "") {
+	this.firstName = firstName;
+	this.lastName = lastName;
+}
+
+function HealthInfo() {
+
+}
+
+function BreedingInfo() {
+
+}
+
+function SellingInfo() {
+
 }
--- profile/fillOutInfo.js
+++ profile/fillOutInfo.js
@@ -2,16 +2,19 @@ $(document).ready(function() {
   // Navigation
 
   // Information
-  var subProfiles = "<ul>";
-  subProfiles += "<li>" + window.testPerson.publicInfo.firstName + " " + window.testPerson.publicInfo.lastName + "</li>";
-  // loop to add dogs
+  var subProfiles = "<ul class=\"nav nav-tabs nav-stacked\">";
+  subProfiles += "<li class=\"active col-sm-12\"><a href=\"#\">" + window.testPerson.publicInfo.firstName + " " + window.testPerson.publicInfo.lastName + "</a></li>";
+  for (i in window.testPerson.dogs) {
+    var dog = window.testPerson.dogs[i];
+    subProfiles += "<li class=\"col-sm-10\"><a href=\"#\">" + dog.generalInfo.firstName + "</a></li>";
+  }
   subProfiles += "</ul>";
   $("#information #subProfiles").html(subProfiles);
 
   // Action
 
   // content
-  $("#content #displayInfo #profilePicture").html("<img src=\"" + window.testPerson.publicInfo.profilePicture + "\">");
+  $("#content #displayInfo #profilePicture").html("<img src=\"" + window.testPerson.publicInfo.profilePicture + "\" width=\"164\" height=\"164\"class=\"img-thumbnail img-md-cropped\">");
   $("#content #displayInfo #name").html(window.testPerson.publicInfo.firstName + " " + window.testPerson.publicInfo.lastName);
-  $("#content #displayInfo #organisation").html(window.testPerson.publicInfo.organisation);
+  $("#content #displayInfo #organisation").html(window.testPerson.publicInfo.organization);
 });
