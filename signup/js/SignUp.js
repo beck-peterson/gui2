8530a863769dd7df6e471c5393fb435108c67143
Alex.Leung <alexleung98@yahoo.com>
2020-04-14 19:41:43 -0400

    Added initial code for Google/Facebook sign in/up.

--- signup/js/SignUp.js
+++ signup/js/SignUp.js
@@ -31,3 +31,49 @@ $(document).ready(function() {
 
 
   });
+
+function signUpWithGoogle(){
+  var provider = new firebase.auth.GoogleAuthProvider();
+
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = result.credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    console.log(user);
+    alert("sign up successful");
+  }).catch(function(error) {
+    // Handle Errors here.
+    alert("sign up unsuccessful")
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
+}
+
+function signUpWithFacebook(){
+  var provider = new firebase.auth.FacebookAuthProvider();
+
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+  // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+  var token = result.credential.accessToken;
+  // The signed-in user info.
+  var user = result.user;
+  console.log(user);
+  alert("sign up successful");
+}).catch(function(error) {
+  // Handle Errors here.
+  alert("sign up unsuccessful")
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+  // ...
+});
+}
