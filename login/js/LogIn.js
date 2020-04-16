be9061bb615bf85125411b84eb3aa912373a9f76
Alex.Leung <alexleung98@yahoo.com>
2020-04-16 02:51:17 -0400

    Added Validation support for Login and Sign up. Also Fixed about page CSS Nav overlapped content

--- login/js/LogIn.js
+++ login/js/LogIn.js
@@ -1,20 +1,31 @@
 $(document).ready(function() {
 // login the user
 const loginForm = document.querySelector("#login");
-loginForm.addEventListener("click", (e) => {
+loginForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  // get user Info
-  const email = loginForm['email'].value;
-  const password = loginForm['password'].value;
 
-  auth.signInWithEmailAndPassword(email, password).then( cred => {
-    console.log(cred.user);
-  }).catch(function(error) {
-    // Handle Errors here. Password must be > 6 characters
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // ...
-    });
+  //before Authentication, check if the fields are valid first before making auth
+  if ($("#signInEmail").valid() && $("#signInPassword").valid()){
+    // get user Info
+    const email = loginForm['signInEmail'].value;
+    const password = loginForm['signInPassword'].value;
+
+    //div content for printing out messages to user regarding login success or fail
+    var element = document.getElementById("loginInfo");
+
+    auth.signInWithEmailAndPassword(email, password).then( cred => {
+      console.log(cred.user);
+      element.innerHTML = "";
+    }).catch(function(error) {
+      // Handle Errors here. Password must be > 6 characters
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      element.innerHTML = "[Email Address or Password is incorrect]";
+      });
+  }else{
+    // fields are invalid
+    console.log("Login credential invalid.")
+  }
 })
 
 // listen for auth state changes - user logs in/out
@@ -54,23 +65,55 @@ function signInWithFacebook(){
   var provider = new auth.FacebookAuthProvider();
 
   firebase.auth().signInWithPopup(provider).then(function(result) {
-  // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-  var token = result.credential.accessToken;
-  // The signed-in user info.
-  var user = result.user;
-  console.log(user);
-  alert("log in successful");
-}).catch(function(error) {
-  // Handle Errors here.
-  console.log(error);
-  alert("log in unsuccesful");
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // The email of the user's account used.
-  var email = error.email;
-  // The firebase.auth.AuthCredential type that was used.
-  var credential = error.credential;
-  // ...
-});
+    // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+    var token = result.credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    console.log(user);
+    alert("log in successful");
+  }).catch(function(error) {
+    // Handle Errors here.
+    console.log(error);
+    alert("log in unsuccesful");
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+    });
 }
+
+$("#login").validate({
+  rules:{
+    signInEmail: {
+      required: true,
+      email: true
+    },
+    signInPassword: {
+      required: true
+    }
+  },
+  messages:{
+    signInEmail:{
+      required: "Please enter an Email",
+      email: "Invalid Email Address"
+    },
+    signInPassword:{
+      required: "Please enter a Password"
+    }
+  },
+  onkeyup: function(element) {
+    var list = $("input");
+    if(element == list[0]){
+      $("#signInEmail").valid();
+      $("#signInPassword").valid();
+    }
+    if(element == list[1]){
+      $("#signInPassword").valid();
+      $("#signInEmail").valid();
+    }
+  }
+});
 });
